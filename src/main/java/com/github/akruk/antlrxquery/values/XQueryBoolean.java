5b292834c219852c2b02bcaf0e78dc81df74f648
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-03-02 05:01:50 +0100

    renamed package

---
+++ src/main/java/com/github/akruk/antlrxquery/values/XQueryBoolean.java
@@ -0,0 +1,34 @@
+package com.github.akruk.antlrxquery.values;
+
+import com.github.akruk.antlrxquery.exceptions.XQueryUnsupportedOperation;
+
+public class XQueryBoolean extends XQueryValueBase<Boolean> {
+    public XQueryBoolean(boolean bool) {
+        value = bool;
+    }
+
+    @Override
+    public Boolean booleanValue() {
+        return value;
+    }
+
+    @Override
+    public XQueryValue not(XQueryValue other) throws XQueryUnsupportedOperation {
+        return new XQueryBoolean(!booleanValue());
+    }
+
+    @Override
+    public XQueryValue and(XQueryValue other) throws XQueryUnsupportedOperation {
+        return new XQueryBoolean(booleanValue() && other.booleanValue());
+    }
+
+    @Override
+    public XQueryValue or(XQueryValue other) throws XQueryUnsupportedOperation {
+        return new XQueryBoolean(booleanValue() || other.booleanValue());
+    }
+
+    @Override
+    public XQueryValue valueEqual(XQueryValue other) throws XQueryUnsupportedOperation {
+        return new XQueryBoolean(booleanValue() == booleanValue());
+    }
+}

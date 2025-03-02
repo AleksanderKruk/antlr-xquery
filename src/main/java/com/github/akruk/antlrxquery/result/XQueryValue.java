f31cbf35dfc94b86d9517d47d9bb7ecbd7d4f381
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-03-02 03:24:31 +0100

    added default implementation for predicates and values to XQueryValue

--- src/main/java/com/github/akruk/antlrxquery/result/XQueryValue.java
+++ src/main/java/com/github/akruk/antlrxquery/result/XQueryValue.java
@@ -2,30 +2,68 @@ package com.github.akruk.antlrxquery.result;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.function.Predicate;
+
+import org.antlr.v4.runtime.tree.ParseTree;
 
 public interface XQueryValue {
   // Function
-  BigDecimal numericValue();
-  String stringValue();
-  boolean booleanValue();
-  List<XQueryValue> collection();
-  XQueryValue filter();
-  XQueryValue reference();
-  XQueryValue add(XQueryValue other);
-  XQueryValue subtract(XQueryValue other);
-  XQueryValue multiply(XQueryValue other);
-  XQueryValue divide(XQueryValue other);
-  XQueryValue integerDivide(XQueryValue other);
-  XQueryValue modulus(XQueryValue other);
-  XQueryValue concatenate(XQueryValue other);
-  XQueryValue valueEqual(XQueryValue other);
-  XQueryValue valueLessThan(XQueryValue other);
-  XQueryValue valueLessEqual(XQueryValue other);
-  XQueryValue valueGreaterThan(XQueryValue other);
-  XQueryValue valueGreaterEqual(XQueryValue other);
-  XQueryValue generalEqual(XQueryValue other);
-  XQueryValue generalLessThan(XQueryValue other);
-  XQueryValue generalLessEqual(XQueryValue other);
-  XQueryValue generalGreaterThan(XQueryValue other);
-  XQueryValue generalGreaterEqual(XQueryValue other);
+  public default BigDecimal numericValue() {
+    return null;
+  };
+
+  public default String stringValue() {
+    return null;
+  };
+
+  public default Boolean booleanValue() {
+    return null;
+  };
+
+  public default List<XQueryValue> collection() {
+    return null;
+  }
+
+  public default Predicate<ParseTree> filter() {
+    return null;
+  }
+
+  public default boolean isNumericValue() {
+    return numericValue() != null;
+  }
+
+  public default boolean isStringValue() {
+    return stringValue() != null;
+  }
+
+  public default boolean isBooleanValue() {
+    return booleanValue() != null;
+  }
+
+  public default boolean isCollection() {
+    return collection() != null;
+  }
+
+  public default boolean isFilter() {
+    return filter() != null;
+  }
+
+  public XQueryValue reference();
+  public XQueryValue add(XQueryValue other);
+  public XQueryValue subtract(XQueryValue other);
+  public XQueryValue multiply(XQueryValue other);
+  public XQueryValue divide(XQueryValue other);
+  public XQueryValue integerDivide(XQueryValue other);
+  public XQueryValue modulus(XQueryValue other);
+  public XQueryValue concatenate(XQueryValue other);
+  public XQueryValue valueEqual(XQueryValue other);
+  public XQueryValue valueLessThan(XQueryValue other);
+  public XQueryValue valueLessEqual(XQueryValue other);
+  public XQueryValue valueGreaterThan(XQueryValue other);
+  public XQueryValue valueGreaterEqual(XQueryValue other);
+  public XQueryValue generalEqual(XQueryValue other);
+  public XQueryValue generalLessThan(XQueryValue other);
+  public XQueryValue generalLessEqual(XQueryValue other);
+  public XQueryValue generalGreaterThan(XQueryValue other);
+  public XQueryValue generalGreaterEqual(XQueryValue other);
 }

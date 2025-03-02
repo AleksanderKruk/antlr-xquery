7c958ac5e1013e104996e17688bae892f54cd351
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-03-02 06:41:43 +0100

    implemented literal evalutation

--- src/main/java/com/github/akruk/antlrxquery/evaluator/XQueryEvaluatorVisitor.java
+++ src/main/java/com/github/akruk/antlrxquery/evaluator/XQueryEvaluatorVisitor.java
@@ -7,9 +7,11 @@ import javax.inject.Inject;
 
 import org.antlr.v4.runtime.Parser;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.TerminalNode;
 
 import com.github.akruk.antlrxquery.AntlrXqueryParserBaseVisitor;
 import com.github.akruk.antlrxquery.AntlrXqueryParser.LiteralContext;
+import com.github.akruk.antlrxquery.AntlrXqueryParser.PrimaryExprContext;
 import com.github.akruk.antlrxquery.values.XQueryNumber;
 import com.github.akruk.antlrxquery.values.XQueryString;
 import com.github.akruk.antlrxquery.values.XQueryValue;
@@ -27,19 +29,24 @@ class XQueryEvaluatorVisitor extends AntlrXqueryParserBaseVisitor<XQueryValue> {
     public XQueryValue visitLiteral(LiteralContext ctx) {
 
         if (ctx.STRING() != null) {
-            String string = unescapeString(ctx.STRING().getText());
+            String text = ctx.getText();
+            String removepars = ctx.getText().substring(1, text.length()-1);
+            String string = unescapeString(removepars);
             return new XQueryString(string);
         }
+
         if (ctx.INTEGER() != null) {
-            // BigDecimal number = ctx.INTEGER().getText();
             return new XQueryNumber(new BigDecimal(ctx.INTEGER().getText()));
         }
+
         return new XQueryNumber(new BigDecimal(ctx.DECIMAL().getText()));
     }
 
     // TODO: ESCAPE
     private String unescapeString(String str) {
-        return str;
+        return str.replace("\"\"", "\"").replace("''", "'");
     }
+
+
 
 }

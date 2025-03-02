a24c43946a9996b20cd721683e9d41a462e13331
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-03-02 06:40:13 +0100

    added tests for basic string literals

--- src/test/java/com/github/akruk/antlrxquery/XQueryTest.java
+++ src/test/java/com/github/akruk/antlrxquery/XQueryTest.java
@@ -2,11 +2,49 @@ package com.github.akruk.antlrxquery;
 
 import org.junit.Test;
 
+import com.github.akruk.antlrxquery.evaluator.XQuery;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class XQueryTest {
+
     @Test
-    public void testApp() {
-        assertEquals(1, 1);
+    public void stringLiteralsDoubleQuote() {
+        String xquery = """
+            "string"
+        """;
+        var value = XQuery.evaluate(null, xquery, null);
+        assertEquals("string", value.stringValue());
     }
+
+    @Test
+    public void stringLiteralsSingleQuote() {
+        String xquery = """
+                    'string'
+                """;
+        var value = XQuery.evaluate(null, xquery, null);
+        assertEquals("string", value.stringValue());
+    }
+
+    @Test
+    public void stringLiteralsEscapeCharsSingle() {
+        String xquery = """
+                    'a''b'
+                """;
+        var value = XQuery.evaluate(null, xquery, null);
+        assertEquals("a'b", value.stringValue());
+    }
+
+    @Test
+    public void stringLiteralsEscapeCharsDouble() {
+        String xquery = """
+                    "a""b"
+                """;
+        var value = XQuery.evaluate(null, xquery, null);
+        assertEquals("a\"b", value.stringValue());
+    }
+
+
+
 }

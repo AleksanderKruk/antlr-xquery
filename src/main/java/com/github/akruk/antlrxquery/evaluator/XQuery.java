52b871ddd334f2ba489854d1eed1c531bd8be416
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-03-03 01:47:35 +0100

    Implemented basic XQuery.evaluate

---
+++ src/main/java/com/github/akruk/antlrxquery/evaluator/XQuery.java
@@ -0,0 +1,23 @@
+package com.github.akruk.antlrxquery.evaluator;
+
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Parser;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CharStream;
+import com.github.akruk.antlrxquery.AntlrXqueryLexer;
+import com.github.akruk.antlrxquery.AntlrXqueryParser;
+import com.github.akruk.antlrxquery.values.XQueryValue;
+
+public final class XQuery {
+  public static XQueryValue evaluate(ParseTree tree, String xquery, Parser parser) {
+    CharStream characters = CharStreams.fromString(xquery);
+    var xqueryLexer = new AntlrXqueryLexer(characters);
+    var xqueryTokens = new CommonTokenStream(xqueryLexer);
+    var xqueryParser = new AntlrXqueryParser(xqueryTokens);
+    var xqueryTree = xqueryParser.xquery();
+    XQueryEvaluatorVisitor visitor = new XQueryEvaluatorVisitor(xqueryTree, parser);
+    XQueryValue evaluated = visitor.visit(xqueryTree);
+    return evaluated;
+  }
+}

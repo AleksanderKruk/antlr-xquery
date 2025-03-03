3b6e48cb2d7401e4da3903a7dda614907521af6f
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-03-03 06:48:06 +0100

    added TRUE and FALSE constants

--- src/main/java/com/github/akruk/antlrxquery/values/XQueryBoolean.java
+++ src/main/java/com/github/akruk/antlrxquery/values/XQueryBoolean.java
@@ -3,6 +3,9 @@ package com.github.akruk.antlrxquery.values;
 import com.github.akruk.antlrxquery.exceptions.XQueryUnsupportedOperation;
 
 public class XQueryBoolean extends XQueryValueBase<Boolean> {
+    public static final XQueryBoolean TRUE = new XQueryBoolean(true);
+    public static final XQueryBoolean FALSE = new XQueryBoolean(false);
+
     public XQueryBoolean(boolean bool) {
         value = bool;
     }

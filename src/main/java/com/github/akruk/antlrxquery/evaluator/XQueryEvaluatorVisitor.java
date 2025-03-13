6d2fcd9e02ae332e26a169598801030e2341f491
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-03-13 22:07:03 +0100

    implemented name testing

--- src/main/java/com/github/akruk/antlrxquery/evaluator/XQueryEvaluatorVisitor.java
+++ src/main/java/com/github/akruk/antlrxquery/evaluator/XQueryEvaluatorVisitor.java
@@ -7,12 +7,18 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import org.antlr.v4.runtime.Parser;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
 import com.github.akruk.antlrxquery.AntlrXqueryParserBaseVisitor;
 import com.github.akruk.antlrxquery.AntlrXqueryParser.AbbrevReverseStepContext;
 import com.github.akruk.antlrxquery.AntlrXqueryParser.ArgumentContext;
@@ -501,6 +507,7 @@ class XQueryEvaluatorVisitor extends AntlrXqueryParserBaseVisitor<XQueryValue> {
         return new XQuerySequence(matchedNodes);
     }
 
+    private Predicate<String> canBeTokenName = Pattern.compile("^[[:upper:]]").asPredicate();
     @Override
     public XQueryValue visitNameTest(NameTestContext ctx) {
         var matchedTreeNodes = matchedTreeNodes();
@@ -523,14 +530,36 @@ class XQueryEvaluatorVisitor extends AntlrXqueryParserBaseVisitor<XQueryValue> {
                 case "*" -> nodeSequence(stepNodes);
                 // case "*:" -> ;
                 // case ":*" -> ;
-                default -> null;
+                default -> throw new AssertionError("Invalid wildcard");
             };
         }
+        matchedTreeNodes = new ArrayList<>(stepNodes.size());
+        for (ParseTree node : stepNodes) {
+            String name = ctx.ID().toString();
+            if (canBeTokenName.test(name)) {
+                // test for tokens
+                int tokenType = parser.getTokenType(name);
+                // TODO: error values
+                if (tokenType == Token.INVALID_TYPE) return null;
+                TerminalNode tokenNode = (TerminalNode) node;
+                Token token = tokenNode.getSymbol();
+                if (token.getType() == tokenType) {
+                    matchedTreeNodes.add(tokenNode);
+                }
+            }
+            else { // test for rule
+                int ruleIndex = parser.getRuleIndex(name);
+                // TODO: error values
+                if (ruleIndex == -1) return null;
+                ParserRuleContext testedRule = (ParserRuleContext) node;
+                if (testedRule.getRuleIndex() == ruleIndex) {
+                    matchedTreeNodes.add(testedRule);
+                }
+            }
+        }
 
-        List<XQueryValue> nodes = stepNodes.stream()
-            .map(XQueryTreeNode::new)
-            .collect(Collectors.toList());
-        return new XQuerySequence(nodes);
+
+        return nodeSequence(stepNodes);
     }
 
     private List<ParseTree> getAllFollowing(List<ParseTree> nodes) {

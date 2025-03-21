76f3562a38ae41a38db5bfd796b15accb9eaad18
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-03-21 19:07:36 +0100

    Added context passsing to functions

--- src/main/java/com/github/akruk/antlrxquery/values/XQueryFunction.java
+++ src/main/java/com/github/akruk/antlrxquery/values/XQueryFunction.java
@@ -2,6 +2,9 @@ package com.github.akruk.antlrxquery.values;
 
 import java.util.List;
 
+import com.github.akruk.antlrxquery.evaluator.XQueryVisitingContext;
+
+
 public interface XQueryFunction {
-    public XQueryValue call(List<XQueryValue> values);
+    public XQueryValue call(final XQueryVisitingContext context, final List<XQueryValue> values);
 }

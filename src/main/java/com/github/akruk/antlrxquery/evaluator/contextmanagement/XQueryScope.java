fecf5cd3ed2ad8a7bce5113e933008fb4df54169
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-03-23 03:49:26 +0100

    implemented Scope interface

--- src/main/java/com/github/akruk/antlrxquery/evaluator/contextmanagement/XQueryScope.java
+++ src/main/java/com/github/akruk/antlrxquery/evaluator/contextmanagement/XQueryScope.java
@@ -1,9 +1,9 @@
 package com.github.akruk.antlrxquery.evaluator.contextmanagement;
 
-import java.util.Map;
-
 import com.github.akruk.antlrxquery.values.XQueryValue;
 
 public interface XQueryScope {
-
+    boolean provideVariable(String variableName, XQueryValue assignedValue);
+    XQueryValue getVariable(String variableName);
+    boolean hasVariable(String variableName);
 }

32dcc4f3962d2a53d07b0bcfcb4e1c390354356a
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-03-23 03:48:52 +0100

    added provideVariable

--- src/main/java/com/github/akruk/antlrxquery/evaluator/contextmanagement/XQueryContext.java
+++ src/main/java/com/github/akruk/antlrxquery/evaluator/contextmanagement/XQueryContext.java
@@ -1,11 +1,11 @@
 package com.github.akruk.antlrxquery.evaluator.contextmanagement;
 
+import com.github.akruk.antlrxquery.values.XQueryValue;
+
 public interface XQueryContext {
-
   void leaveScope();
-
-void enterScope();
-
-XQueryScope currentScope();
-
+  void enterScope();
+  XQueryScope currentScope();
+  boolean provideVariable(String variableName, XQueryValue assignedValue);
+  XQueryValue getVariable(String variableName);
 }

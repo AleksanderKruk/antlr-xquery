b88f198213f4abd9e9fcdbd828a066ada69e0b11
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-03-24 22:41:22 +0100

    implemented base value factory

---
+++ src/main/java/com/github/akruk/antlrxquery/values/factories/defaults/XQueryBaseValueFactory.java
@@ -0,0 +1,62 @@
+package com.github.akruk.antlrxquery.values.factories.defaults;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import com.github.akruk.antlrxquery.values.XQueryBoolean;
+import com.github.akruk.antlrxquery.values.XQueryFunction;
+import com.github.akruk.antlrxquery.values.XQueryFunctionReference;
+import com.github.akruk.antlrxquery.values.XQueryNumber;
+import com.github.akruk.antlrxquery.values.XQuerySequence;
+import com.github.akruk.antlrxquery.values.XQueryString;
+import com.github.akruk.antlrxquery.values.XQueryTreeNode;
+import com.github.akruk.antlrxquery.values.XQueryValue;
+import com.github.akruk.antlrxquery.values.factories.XQueryValueFactory;
+
+public class XQueryBaseValueFactory implements XQueryValueFactory{
+    private Map<ParseTree, XQueryValue> createdNodes = new HashMap<>();
+    private Map<BigDecimal, XQueryValue> createdNumbers = new HashMap<>();
+    private Map<String, XQueryValue> createdStrings = new HashMap<>();
+    private Map<List<XQueryValue>, XQueryValue> createdSequences = new HashMap<>();
+    @Override
+    public XQueryValue bool(boolean v) {
+        return XQueryBoolean.of(v);
+    }
+
+    @Override
+    public XQueryValue functionReference(XQueryFunction f) {
+        return new XQueryFunctionReference(f);
+    }
+
+    @Override
+    public XQueryValue node(ParseTree v) {
+        XQueryValue returnedNode = createdNodes.computeIfAbsent(v, t -> new XQueryTreeNode(v));
+        return returnedNode;
+    }
+
+    @Override
+    public XQueryValue number(BigDecimal d) {
+        XQueryValue returnedNumber = createdNumbers.computeIfAbsent(d, t -> new XQueryNumber(d));
+        return returnedNumber;
+    }
+
+    @Override
+    public XQueryValue sequence(List<XQueryValue> v) {
+        XQueryValue returnedSequence = createdSequences.computeIfAbsent(v, t -> new XQuerySequence(v));
+        return returnedSequence;
+    }
+
+    @Override
+    public XQueryValue string(String s) {
+        XQueryValue returnedString = createdStrings.computeIfAbsent(s, t -> new XQueryString(s));
+        return returnedString;
+    }
+
+
+}

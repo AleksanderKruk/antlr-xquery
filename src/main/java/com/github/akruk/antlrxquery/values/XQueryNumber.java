77f0685f09da2f5ef72883bbf65439708373ce03
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-03-24 23:04:42 +0100

    implemented XQueryNumber with factories

--- src/main/java/com/github/akruk/antlrxquery/values/XQueryNumber.java
+++ src/main/java/com/github/akruk/antlrxquery/values/XQueryNumber.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.math.MathContext;
 
 import com.github.akruk.antlrxquery.exceptions.XQueryUnsupportedOperation;
+import com.github.akruk.antlrxquery.values.factories.XQueryValueFactory;
 
 public class XQueryNumber extends XQueryValueBase<BigDecimal> {
     public static final XQueryNumber ZERO = new XQueryNumber(BigDecimal.ZERO);
@@ -35,58 +36,62 @@ public class XQueryNumber extends XQueryValueBase<BigDecimal> {
     }
 
     @Override
-    public XQueryValue add(XQueryValue other) throws XQueryUnsupportedOperation {
+    public XQueryValue add(XQueryValueFactory valueFactory, XQueryValue other) throws XQueryUnsupportedOperation {
         return new XQueryNumber(value.add(other.numericValue(), MathContext.UNLIMITED));
     }
 
     @Override
-    public XQueryValue subtract(XQueryValue other) throws XQueryUnsupportedOperation {
+    public XQueryValue subtract(XQueryValueFactory valueFactory, XQueryValue other) throws XQueryUnsupportedOperation {
         return new XQueryNumber(value.subtract(other.numericValue(), MathContext.UNLIMITED));
     }
 
     @Override
-    public XQueryValue multiply(XQueryValue other) throws XQueryUnsupportedOperation {
+    public XQueryValue multiply(XQueryValueFactory valueFactory, XQueryValue other) throws XQueryUnsupportedOperation {
         return new XQueryNumber(value.multiply(other.numericValue(), MathContext.UNLIMITED));
     }
 
     @Override
-    public XQueryValue divide(XQueryValue other) throws XQueryUnsupportedOperation {
+    public XQueryValue divide(XQueryValueFactory valueFactory, XQueryValue other) throws XQueryUnsupportedOperation {
         return new XQueryNumber(value.divide(other.numericValue(), MathContext.UNLIMITED));
     }
 
     @Override
-    public XQueryValue integerDivide(XQueryValue other) throws XQueryUnsupportedOperation {
+    public XQueryValue integerDivide(XQueryValueFactory valueFactory, XQueryValue other) throws XQueryUnsupportedOperation {
         return new XQueryNumber(value.divideToIntegralValue(other.numericValue()));
     }
 
     @Override
-    public XQueryValue modulus(XQueryValue other) throws XQueryUnsupportedOperation {
+    public XQueryValue modulus(XQueryValueFactory valueFactory, XQueryValue other) throws XQueryUnsupportedOperation {
         return new XQueryNumber(value.remainder(other.numericValue()));
     }
 
     @Override
-    public XQueryValue valueEqual(XQueryValue other) {
+    public XQueryValue valueEqual(XQueryValueFactory valueFactory, XQueryValue other) {
         if (!other.isNumericValue())
             return XQueryBoolean.FALSE;
         return XQueryBoolean.of(value.compareTo(other.numericValue()) == 0);
     }
 
     @Override
-    public XQueryValue valueLessThan(XQueryValue other) {
+    public XQueryValue valueLessThan(XQueryValueFactory valueFactory, XQueryValue other) {
         if (!other.isNumericValue())
             return XQueryBoolean.FALSE;
         return XQueryBoolean.of(value.compareTo(other.numericValue()) == -1);
     }
 
-
     @Override
-    public XQueryValue copy() {
-        return new XQueryNumber(value.plus());
+    public XQueryValue copy(XQueryValueFactory valueFactory) {
+        return valueFactory.number(value);
     }
 
     @Override
-    public XQueryValue data() throws XQueryUnsupportedOperation {
+    public XQueryValue data(XQueryValueFactory valueFactory) throws XQueryUnsupportedOperation {
         var atomized = atomize();
         return new XQuerySequence(atomized);
+    }
+
+    @Override
+    public XQueryValue empty(XQueryValueFactory valueFactory) {
+        return valueFactory.bool(false);
     }
 }

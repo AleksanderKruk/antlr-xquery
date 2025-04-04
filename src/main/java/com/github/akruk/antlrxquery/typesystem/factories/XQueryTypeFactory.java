99af0bf5ff65c12fa770cf7d55ca233020c08b27
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-04-04 13:15:56 +0200

    implemented type factory

---
+++ src/main/java/com/github/akruk/antlrxquery/typesystem/factories/XQueryTypeFactory.java
@@ -0,0 +1,16 @@
+package com.github.akruk.antlrxquery.typesystem.factories;
+
+import com.github.akruk.antlrxquery.typesystem.XQueryType;
+
+public interface XQueryTypeFactory {
+    public XQueryType string();
+    public XQueryType number();
+    public XQueryType integer();
+    public XQueryType anyNode();
+    public XQueryType anyArray();
+    public XQueryType anyMap();
+    public XQueryType anyElement();
+    public XQueryType anyFunction();
+    public XQueryType anyItem();
+    public XQueryType boolean_();
+}

e22cc3a76937e0f466fdc2e75962a79bc13fc390
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-04-13 00:10:31 +0200

    implemented 'any function' item

--- src/main/java/com/github/akruk/antlrxquery/typesystem/defaults/XQueryEnumItemTypeAnyFunction.java
+++ src/main/java/com/github/akruk/antlrxquery/typesystem/defaults/XQueryEnumItemTypeAnyFunction.java
@@ -3,7 +3,7 @@ package com.github.akruk.antlrxquery.typesystem.defaults;
 public class XQueryEnumItemTypeAnyFunction extends XQueryEnumItemType {
 
   public XQueryEnumItemTypeAnyFunction() {
-    super(XQueryTypes.ANY_FUNCTION, null, null, null);
+    super(XQueryTypes.ANY_FUNCTION, null, null, null, null, null, null);
   }
 
 }

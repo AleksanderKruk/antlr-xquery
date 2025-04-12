a927ed5d1c86b5e7d799293d78eb32b73940383d
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-04-13 00:09:55 +0200

    implemented 'any item' item

--- src/main/java/com/github/akruk/antlrxquery/typesystem/defaults/XQueryEnumItemTypeAnyItem.java
+++ src/main/java/com/github/akruk/antlrxquery/typesystem/defaults/XQueryEnumItemTypeAnyItem.java
@@ -3,7 +3,7 @@ package com.github.akruk.antlrxquery.typesystem.defaults;
 public class XQueryEnumItemTypeAnyItem extends XQueryEnumItemType {
 
   public XQueryEnumItemTypeAnyItem() {
-    super(XQueryTypes.ANY_ITEM, null, null, null);
+    super(XQueryTypes.ANY_ITEM, null, null, null, null, null, null);
   }
 
 }

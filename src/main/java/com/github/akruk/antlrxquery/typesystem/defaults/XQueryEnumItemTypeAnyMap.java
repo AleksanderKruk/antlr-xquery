ec62e77b8b06b478fc620178a2a0e43f9fca6e0f
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-04-13 00:10:56 +0200

    implemented 'any map' item

--- src/main/java/com/github/akruk/antlrxquery/typesystem/defaults/XQueryEnumItemTypeAnyMap.java
+++ src/main/java/com/github/akruk/antlrxquery/typesystem/defaults/XQueryEnumItemTypeAnyMap.java
@@ -3,7 +3,7 @@ package com.github.akruk.antlrxquery.typesystem.defaults;
 public class XQueryEnumItemTypeAnyMap extends XQueryEnumItemType {
 
   public XQueryEnumItemTypeAnyMap() {
-    super(XQueryTypes.ANY_MAP, null, null, null);
+    super(XQueryTypes.ANY_MAP, null, null, null, null, null, null);
   }
 
 }

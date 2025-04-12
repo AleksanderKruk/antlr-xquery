f3a93363c6b44a4de3f70bbe2b14dd09d7109d4b
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-04-13 00:10:50 +0200

    implemented 'element' item

--- src/main/java/com/github/akruk/antlrxquery/typesystem/defaults/XQueryEnumItemTypeElement.java
+++ src/main/java/com/github/akruk/antlrxquery/typesystem/defaults/XQueryEnumItemTypeElement.java
@@ -3,7 +3,7 @@ package com.github.akruk.antlrxquery.typesystem.defaults;
 public class XQueryEnumItemTypeElement extends XQueryEnumItemType {
 
   public XQueryEnumItemTypeElement(String elementName) {
-    super(XQueryTypes.ANY_ELEMENT, null, null, elementName);
+    super(XQueryTypes.ANY_ELEMENT, null, null, null, null, null, elementName);
   }
 
 }

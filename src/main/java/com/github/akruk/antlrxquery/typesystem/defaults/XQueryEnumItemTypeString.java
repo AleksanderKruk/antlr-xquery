5c546dcaa855c803bcd2ea4f499d871037b59a32
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-04-13 00:11:34 +0200

    implemented 'string' item

--- src/main/java/com/github/akruk/antlrxquery/typesystem/defaults/XQueryEnumItemTypeString.java
+++ src/main/java/com/github/akruk/antlrxquery/typesystem/defaults/XQueryEnumItemTypeString.java
@@ -3,7 +3,7 @@ package com.github.akruk.antlrxquery.typesystem.defaults;
 public class XQueryEnumItemTypeString extends XQueryEnumItemType {
 
   public XQueryEnumItemTypeString() {
-    super(XQueryTypes.STRING, null, null, null);
+    super(XQueryTypes.STRING, null, null, null, null, null, null);
   }
 
 }

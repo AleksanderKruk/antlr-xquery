12953161fe54a51aa0257cd3f2b2f726166cce80
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-04-17 01:07:05 +0200

    added operator type merging methods to interface

--- src/main/java/com/github/akruk/antlrxquery/typesystem/XQuerySequenceType.java
+++ src/main/java/com/github/akruk/antlrxquery/typesystem/XQuerySequenceType.java
@@ -1,9 +1,12 @@
 package com.github.akruk.antlrxquery.typesystem;
 
 public interface XQuerySequenceType {
-    // subtype(A, B) judgement
     XQueryItemType getItemType();
     XQuerySequenceType sequenceMerge(XQuerySequenceType other);
+    XQuerySequenceType unionMerge(XQuerySequenceType other);
+    XQuerySequenceType intersectionMerge(XQuerySequenceType other);
+    XQuerySequenceType exceptionMerge(XQuerySequenceType other);
+    // subtype(A, B) judgement
     boolean isSubtypeOf(XQuerySequenceType obj);
     boolean itemtypeIsSubtypeOf(XQuerySequenceType obj);
     boolean isOne();

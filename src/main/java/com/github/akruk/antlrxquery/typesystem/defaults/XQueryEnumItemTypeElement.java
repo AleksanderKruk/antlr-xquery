74531d13d17f4c1b149c8a1e7bdecedf43a67f14
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-05-15 19:48:11 +0200

    changed constructor arg to Set<String>

--- src/main/java/com/github/akruk/antlrxquery/typesystem/defaults/XQueryEnumItemTypeElement.java
+++ src/main/java/com/github/akruk/antlrxquery/typesystem/defaults/XQueryEnumItemTypeElement.java
@@ -1,8 +1,10 @@
 package com.github.akruk.antlrxquery.typesystem.defaults;
 
+import java.util.Set;
+
 public class XQueryEnumItemTypeElement extends XQueryEnumItemType {
 
-  public XQueryEnumItemTypeElement(String elementName) {
+  public XQueryEnumItemTypeElement(Set<String> elementName) {
     super(XQueryTypes.ELEMENT, null, null, null, null, null, elementName);
   }
 

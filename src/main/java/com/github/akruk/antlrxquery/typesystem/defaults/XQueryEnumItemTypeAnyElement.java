7e2578258632a0258779c2ba6b36cfcc6374092a
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-05-15 18:49:06 +0200

    changed to use any node instead of any element

--- src/main/java/com/github/akruk/antlrxquery/typesystem/defaults/XQueryEnumItemTypeAnyElement.java
+++
@@ -1,9 +0,0 @@
-package com.github.akruk.antlrxquery.typesystem.defaults;
-
-public class XQueryEnumItemTypeAnyElement extends XQueryEnumItemType {
-
-  public XQueryEnumItemTypeAnyElement() {
-    super(XQueryTypes.ANY_ELEMENT, null, null, null, null, null, null);
-  }
-
-}

e29783356c0ebb76da17e8750b1a4f3415c59402
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-05-16 05:01:07 +0200

    placeholder enum type

---
+++ src/main/java/com/github/akruk/antlrxquery/typesystem/defaults/XQueryEnumItemTypeError.java
@@ -0,0 +1,9 @@
+package com.github.akruk.antlrxquery.typesystem.defaults;
+
+public class XQueryEnumItemTypeError extends XQueryEnumItemType {
+
+  public XQueryEnumItemTypeError() {
+    super(XQueryTypes.ERROR, null, null, null, null, null, null);
+  }
+
+}

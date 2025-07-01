63a3f641067ee7ec8137a22e90b3731f2d962dff
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-07-01 15:02:10 +0200

    changed List to Collection

--- src/main/java/com/github/akruk/antlrxquery/typesystem/defaults/XQueryChoiceItemType.java
+++ src/main/java/com/github/akruk/antlrxquery/typesystem/defaults/XQueryChoiceItemType.java
@@ -1,13 +1,12 @@
 package com.github.akruk.antlrxquery.typesystem.defaults;
 
 import java.util.Collection;
-import java.util.List;
 
 import com.github.akruk.antlrxquery.typesystem.XQueryItemType;
 import com.github.akruk.antlrxquery.typesystem.factories.XQueryTypeFactory;
 
 public class XQueryChoiceItemType extends XQueryEnumItemType {
-    public XQueryChoiceItemType(List<XQueryItemType> itemTypes, XQueryTypeFactory factory) {
+    public XQueryChoiceItemType(Collection<XQueryItemType> itemTypes, XQueryTypeFactory factory) {
         super(XQueryTypes.CHOICE, null, null, null, null, null, null, factory, itemTypes);
     }
 

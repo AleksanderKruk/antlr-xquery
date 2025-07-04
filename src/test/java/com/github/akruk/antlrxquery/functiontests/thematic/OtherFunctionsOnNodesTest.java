9b27e93c4497f8244e514a37a99cbbd6e02e8e0c
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-07-04 17:08:33 +0200

    moved 'other functions on nodeds' functions to separate testing class

---
+++ src/test/java/com/github/akruk/antlrxquery/functiontests/thematic/OtherFunctionsOnNodesTest.java
@@ -0,0 +1,136 @@
+package com.github.akruk.antlrxquery.functiontests.thematic;
+
+import org.junit.jupiter.api.Test;
+
+import com.github.akruk.antlrxquery.functiontests.FunctionsSemanticTest;
+
+public class OtherFunctionsOnNodesTest extends FunctionsSemanticTest{
+
+    @Test
+    public void name_default() {
+        assertType("fn:name()",typeFactory.one(typeFactory.itemString()));
+    }
+
+    // @Test
+    // public void name_node() {
+    //     assertNoErrors(analyze("fn:name(<b/>)"));
+    // }
+
+    @Test
+    public void name_invalid() {
+        assertErrors("fn:name( true() )");
+    }
+
+    @Test
+    public void localName_default() {
+        assertType("fn:local-name()",typeFactory.one(typeFactory.itemString()));
+    }
+
+    // @Test
+    // public void localName_node() {
+    //     assertNoErrors(analyze("fn:local-name(<ns:a xmlns:ns='u'/>)"));
+    // }
+
+    @Test
+    public void localName_bad() {
+        assertErrors("fn:local-name(123)");
+    }
+
+    @Test
+    public void namespaceUri_default() {
+        assertType("fn:namespace-uri()",typeFactory.one(typeFactory.itemString()));
+    }
+
+    // @Test
+    // public void namespaceUri_node() {
+    //     assertNoErrors(analyze("fn:namespace-uri(<ns:a xmlns:ns='u'/>)"));
+    // }
+
+    @Test
+    public void namespaceUri_bad() {
+        assertErrors("fn:namespace-uri(1)");
+    }
+
+    @Test
+    public void lang_positional() {
+        assertType("fn:lang('en')",typeFactory.one(typeFactory.itemBoolean()));
+    }
+
+    // @Test
+    // public void lang_namedNode() {
+    //     var r = analyze("fn:lang(language := 'pl', node := <a xml:lang='pl'/>)");
+    //     assertNoErrors(r);
+    // }
+
+    // @Test
+    // public void lang_missingLang() {
+    //     assertErrors("fn:lang()");
+    // }
+
+    @Test
+    public void lang_badLangType() {
+        assertErrors("fn:lang(1)");
+    }
+
+    @Test
+    public void root_default() {
+        assertType("fn:root()", typeFactory.zeroOrOne(typeFactory.itemAnyNode()));
+    }
+
+    // @Test
+    // public void root_node() {
+    //     assertNoErrors(analyze("fn:root(<a><b/></a>)"));
+    // }
+
+    @Test
+    public void root_wrong() {
+        assertErrors("fn:root('x')");
+    }
+
+    @Test
+    public void path_default() {
+        assertType("fn:path()", typeFactory.zeroOrOne(typeFactory.itemString()));
+    }
+
+    // @Test
+    // public void path_withOptions() {
+    //     var r = analyze("fn:path(., map{})");
+    //     assertNoErrors(r);
+    // }
+
+    @Test
+    public void path_bad() {
+        assertErrors("fn:path(1,2)");
+    }
+
+    @Test
+    public void hasChildren_default() {
+        assertType("fn:has-children()",typeFactory.one(typeFactory.itemBoolean()));
+    }
+
+    // @Test
+    // public void hasChildren_node() {
+    //     assertNoErrors(analyze("fn:has-children(<a><b/></a>)"));
+    // }
+
+    @Test
+    public void hasChildren_invalid() {
+        assertErrors("fn:has-children(1)");
+    }
+
+    @Test
+    public void siblings_default() {
+        assertType("fn:siblings()",typeFactory.zeroOrMore(typeFactory.itemAnyNode()));
+    }
+
+    // @Test
+    // public void siblings_node() {
+    //     assertNoErrors(analyze("fn:siblings(<a/><b/>)"));
+    // }
+
+    @Test
+    public void siblings_wrong() {
+        assertErrors("fn:siblings('x')");
+    }
+
+}

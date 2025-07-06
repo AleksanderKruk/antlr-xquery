c0017ef478e0d3d7979fdef941acfe99d9504d24
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-07-06 13:58:06 +0200

    formatting

--- src/main/java/com/github/akruk/antlrxquery/namespaceresolver/INamespaceResolver.java
+++ src/main/java/com/github/akruk/antlrxquery/namespaceresolver/INamespaceResolver.java
@@ -3,7 +3,5 @@ package com.github.akruk.antlrxquery.namespaceresolver;
 import com.github.akruk.antlrxquery.namespaceresolver.NamespaceResolver.ResolvedName;
 
 public interface INamespaceResolver {
-
     ResolvedName resolve(String fullName);
-
 }

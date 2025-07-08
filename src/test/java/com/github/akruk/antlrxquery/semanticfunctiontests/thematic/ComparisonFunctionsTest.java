ee34f329f25da4607105f2666725345315a29016
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-07-08 06:06:48 +0200

    reformatted

--- src/test/java/com/github/akruk/antlrxquery/semanticfunctiontests/thematic/ComparisonFunctionsTest.java
+++ src/test/java/com/github/akruk/antlrxquery/semanticfunctiontests/thematic/ComparisonFunctionsTest.java
@@ -6,154 +6,244 @@ import com.github.akruk.antlrxquery.semanticfunctiontests.FunctionsSemanticTest;
 
 public class ComparisonFunctionsTest extends FunctionsSemanticTest {
 
-    // fn:atomic-equal($v1 as xs:anyAtomicType, $v2 as xs:anyAtomicType) as xs:boolean
-    @Test public void atomicEqual_valid() {
+    // fn:atomic-equal($v1 as xs:anyAtomicType, $v2 as xs:anyAtomicType) as
+    // xs:boolean
+    @Test
+    public void atomicEqual_valid()
+    {
         assertType("fn:atomic-equal(1, 'a')",
             typeFactory.boolean_());
     }
-    @Test public void atomicEqual_wrongTypes() {
-        assertErrors("fn:atomic-equal(<a/>, 1)");
-        assertErrors("fn:atomic-equal(true(), <b/>)");
-    }
-    @Test public void atomicEqual_arityErrors() {
+
+    @Test
+    public void atomicEqual_arityErrors()
+    {
         assertErrors("fn:atomic-equal(1)");
         assertErrors("fn:atomic-equal()");
         assertErrors("fn:atomic-equal(1,2,3)");
     }
 
-    // fn:deep-equal($in1 as item()*, $in2 as item()*, $options as (xs:string|map(*))? := {}) as xs:boolean
-    @Test public void deepEqual_minimal() {
+    // fn:deep-equal($in1 as item()*, $in2 as item()*, $options as
+    // (xs:string|map(*))? := {}) as xs:boolean
+    @Test
+    public void deepEqual_minimal()
+    {
         assertType("fn:deep-equal((1,'x'),(1,'x'))",
             typeFactory.boolean_());
     }
-    @Test public void deepEqual_withStringOptions() {
+
+    @Test
+    public void deepEqual_withStringOptions()
+    {
         assertType("fn:deep-equal((1),(1),'collation=uc')",
             typeFactory.boolean_());
     }
-    @Test public void deepEqual_withMapOptions() {
+
+    @Test
+    public void deepEqual_withMapOptions()
+    {
         assertType("fn:deep-equal((<a/>),(<a/>), map{})",
             typeFactory.boolean_());
     }
-    @Test public void deepEqual_missingOrExtra() {
+
+    @Test
+    public void deepEqual_missingOrExtra()
+    {
         assertErrors("fn:deep-equal((1))");
         assertErrors("fn:deep-equal()");
         assertErrors("fn:deep-equal((1),(1), 'opt', 'x')");
     }
-    @Test public void deepEqual_badOptionType() {
+
+    @Test
+    public void deepEqual_badOptionType()
+    {
         assertErrors("fn:deep-equal((1),(1), 123)");
     }
 
-    // fn:compare($v1 as xs:anyAtomicType?,$v2 as xs:anyAtomicType?, $collation as xs:string? := default) as xs:integer?
-    @Test public void compare_noArgs() {
+    // fn:compare($v1 as xs:anyAtomicType?,$v2 as xs:anyAtomicType?, $collation as
+    // xs:string? := default) as xs:integer?
+    @Test
+    public void compare_noArgs()
+    {
         assertType("fn:compare()",
             typeFactory.zeroOrOne(typeFactory.itemNumber()));
     }
-    @Test public void compare_minimal() {
+
+    @Test
+    public void compare_minimal()
+    {
         assertType("fn:compare('a','b')",
             typeFactory.zeroOrOne(typeFactory.itemNumber()));
     }
-    @Test public void compare_withCollation() {
+
+    @Test
+    public void compare_withCollation()
+    {
         assertType("fn:compare('a','b','uci')",
             typeFactory.zeroOrOne(typeFactory.itemNumber()));
     }
-    @Test public void compare_namedCollation() {
+
+    @Test
+    public void compare_namedCollation()
+    {
         assertType("fn:compare(v2 := 'b', v1 := 'a', collation := 'uc')",
             typeFactory.zeroOrOne(typeFactory.itemNumber()));
     }
-    @Test public void compare_wrongTypes() {
+
+    @Test
+    public void compare_wrongTypes()
+    {
         assertErrors("fn:compare(<a/>, 'b')");
         assertErrors("fn:compare('a', <b/>)");
         assertErrors("fn:compare('a','b', 1)");
     }
 
-    // fn:distinct-values($values as xs:anyAtomicType*, $collation as xs:string? := default) as xs:anyAtomicType*
-    @Test public void distinctValues_empty() {
+    // fn:distinct-values($values as xs:anyAtomicType*, $collation as xs:string? :=
+    // default) as xs:anyAtomicType*
+    @Test
+    public void distinctValues_empty()
+    {
         assertType("fn:distinct-values()",
             typeFactory.zeroOrMore(typeFactory.itemAnyItem()));
     }
-    @Test public void distinctValues_withValues() {
+
+    @Test
+    public void distinctValues_withValues()
+    {
         assertType("fn:distinct-values('a','b','a')",
             typeFactory.zeroOrMore(typeFactory.itemAnyItem()));
     }
-    @Test public void distinctValues_namedCollation() {
+
+    @Test
+    public void distinctValues_namedCollation()
+    {
         assertType("fn:distinct-values(1,2,1, collation := 'uc')",
             typeFactory.zeroOrMore(typeFactory.itemAnyItem()));
     }
-    @Test public void distinctValues_wrongType() {
+
+    @Test
+    public void distinctValues_wrongType()
+    {
         assertErrors("fn:distinct-values(<x/>)");
     }
 
-    // fn:duplicate-values($values as xs:anyAtomicType*, $collation as xs:string? := default) as xs:anyAtomicType*
-    @Test public void duplicateValues_empty() {
+    // fn:duplicate-values($values as xs:anyAtomicType*, $collation as xs:string? :=
+    // default) as xs:anyAtomicType*
+    @Test
+    public void duplicateValues_empty()
+    {
         assertType("fn:duplicate-values()",
             typeFactory.zeroOrMore(typeFactory.itemAnyItem()));
     }
-    @Test public void duplicateValues_withValues() {
+
+    @Test
+    public void duplicateValues_withValues()
+    {
         assertType("fn:duplicate-values('x','x','y')",
             typeFactory.zeroOrMore(typeFactory.itemAnyItem()));
     }
-    @Test public void duplicateValues_wrongType() {
+
+    @Test
+    public void duplicateValues_wrongType()
+    {
         assertErrors("fn:duplicate-values(<x/>)");
     }
 
-    // fn:index-of($in as xs:anyAtomicType*, $target as xs:anyAtomicType, $collation as xs:string? := default) as xs:integer*
-    @Test public void indexOf_minimal() {
+    // fn:index-of($in as xs:anyAtomicType*, $target as xs:anyAtomicType, $collation
+    // as xs:string? := default) as xs:integer*
+    @Test
+    public void indexOf_minimal()
+    {
         assertType("fn:index-of(1, 2, 'uc')",
             typeFactory.zeroOrMore(typeFactory.itemNumber()));
     }
-    @Test public void indexOf_defaultCollation() {
+
+    @Test
+    public void indexOf_defaultCollation()
+    {
         assertType("fn:index-of('a','a')",
             typeFactory.zeroOrMore(typeFactory.itemNumber()));
     }
-    @Test public void indexOf_missingOrExtra() {
+
+    @Test
+    public void indexOf_missingOrExtra()
+    {
         assertErrors("fn:index-of(1)");
         assertErrors("fn:index-of()");
         assertErrors("fn:index-of(1,2,'x','y')");
     }
-    @Test public void indexOf_wrongTypes() {
+
+    @Test
+    public void indexOf_wrongTypes()
+    {
         assertErrors("fn:index-of(<a/>,1)");
         assertErrors("fn:index-of(1,<b/>)");
     }
 
-    // fn:starts-with-subsequence($in as item()*, $sub as item()*, $cmp as fn(item(),item())? := fn:deep-equal#2) as xs:boolean
-    @Test public void startsWithSub_minimal() {
+    // fn:starts-with-subsequence($in as item()*, $sub as item()*, $cmp as
+    // fn(item(),item())? := fn:deep-equal#2) as xs:boolean
+    @Test
+    public void startsWithSub_minimal()
+    {
         assertType("fn:starts-with-subsequence((1,2,3),(1,2))",
             typeFactory.boolean_());
     }
-    @Test public void startsWithSub_namedCompare() {
+
+    @Test
+    public void startsWithSub_namedCompare()
+    {
         assertType("fn:starts-with-subsequence((1),(1), compare)",
             typeFactory.boolean_());
     }
-    @Test public void startsWithSub_missingOrWrong() {
+
+    @Test
+    public void startsWithSub_missingOrWrong()
+    {
         assertErrors("fn:starts-with-subsequence((1))");
         assertErrors("fn:starts-with-subsequence()");
         assertErrors("fn:starts-with-subsequence((1),(1),'x')");
     }
 
     // fn:ends-with-subsequence(...) as xs:boolean
-    @Test public void endsWithSub_minimal() {
+    @Test
+    public void endsWithSub_minimal()
+    {
         assertType("fn:ends-with-subsequence((a,b),(b))",
             typeFactory.boolean_());
     }
-    @Test public void endsWithSub_namedArgs() {
+
+    @Test
+    public void endsWithSub_namedArgs()
+    {
         assertType("fn:ends-with-subsequence(input := (1,2), subsequence := (2))",
             typeFactory.boolean_());
     }
-    @Test public void endsWithSub_errors() {
+
+    @Test
+    public void endsWithSub_errors()
+    {
         assertErrors("fn:ends-with-subsequence((1))");
         assertErrors("fn:ends-with-subsequence()");
     }
 
     // fn:contains-subsequence(...) as xs:boolean
-    @Test public void containsSub_minimal() {
+    @Test
+    public void containsSub_minimal()
+    {
         assertType("fn:contains-subsequence((1,2,3),(2,3))",
             typeFactory.boolean_());
     }
-    @Test public void containsSub_namedCompare() {
+
+    @Test
+    public void containsSub_namedCompare()
+    {
         assertType("fn:contains-subsequence((1,2),(1), compare)",
             typeFactory.boolean_());
     }
-    @Test public void containsSub_errors() {
+
+    @Test
+    public void containsSub_errors()
+    {
         assertErrors("fn:contains-subsequence((1))");
         assertErrors("fn:contains-subsequence()");
     }

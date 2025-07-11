8a65bdeabe91a0d6f2507a57cdd77c8714f8c6c9
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-07-11 09:00:45 +0200

    added test for bracketed array constructor

--- src/test/java/com/languagefeaturessemantics/ArraysTest.java
+++ src/test/java/com/languagefeaturessemantics/ArraysTest.java
@@ -15,10 +15,20 @@ public class ArraysTest extends XQuerySemanticAnalyzerTest {
     }
 
     @Test
-    public void oneTypeNonEmptyArrays() {
+    public void named_oneTypeNonEmptyArrays() {
         final var numToNum = typeFactory.array(typeFactory.number());
         final var strToNum = typeFactory.array(typeFactory.enum_(Set.of("a", "b", "c")));
+        assertType("array { 1 }", numToNum);
         assertType("array { 1, 2, 3}", numToNum);
         assertType("array { 'a', 'b', 'c' }", strToNum);
+    }
+
+    @Test
+    public void bracketed_oneTypeNonEmptyArrays() {
+        final var numToNum = typeFactory.array(typeFactory.number());
+        final var strToNum = typeFactory.array(typeFactory.enum_(Set.of("a", "b", "c")));
+        assertType("[ 1 ]", numToNum);
+        assertType("[ 1, 2, 3]", numToNum);
+        assertType("[ 'a', 'b', 'c' ]", strToNum);
     }
 }

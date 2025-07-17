44c2774be669f7309203aeabbf114ba732349d48
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-07-17 05:26:35 +0200

    added test class

---
+++ src/test/java/com/github/akruk/antlrxquery/languagefeatures/evaluation/functions/RandomNumberFunctions.java
@@ -0,0 +1,12 @@
+package com.github.akruk.antlrxquery.languagefeatures.evaluation.functions;
+
+import com.github.akruk.antlrxquery.languagefeatures.evaluation.EvaluationTestsBase;
+
+public class RandomNumberFunctions extends EvaluationTestsBase {
+    // @Test
+    // void random-number-generator() {
+
+
+    // }
+
+}

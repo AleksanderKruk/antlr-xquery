90c95450885fa22a48b0798ae35eb0e6833cd36f
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-07-18 16:05:46 +0200

    removed placeholdering

--- src/main/java/com/github/akruk/antlrxquery/contextmanagement/semanticcontext/XQuerySemanticContextManager.java
+++ src/main/java/com/github/akruk/antlrxquery/contextmanagement/semanticcontext/XQuerySemanticContextManager.java
@@ -5,7 +5,5 @@ import com.github.akruk.antlrxquery.typesystem.XQuerySequenceType;
 
 public interface XQuerySemanticContextManager extends XQueryContextManager {
     boolean entypeVariable(String variableName, XQuerySequenceType assignedType);
-    boolean placeholderVariable(String variableName);
-    boolean restrainVariable(String variableName, XQuerySequenceType type);
     XQuerySequenceType getVariable(String variableName);
 }

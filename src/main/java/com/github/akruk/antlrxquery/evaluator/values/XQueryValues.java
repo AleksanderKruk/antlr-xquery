45fec8d52f6805b7c8e1405ba519f93a77f8c976
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-07-21 18:50:09 +0200

    added node sequence

---
+++ src/main/java/com/github/akruk/antlrxquery/evaluator/values/XQueryValues.java
@@ -0,0 +1,15 @@
+package com.github.akruk.antlrxquery.evaluator.values;
+
+public enum XQueryValues {
+    NUMBER,
+    STRING,
+    BOOLEAN,
+    NODE,
+    ARRAY,
+    MAP,
+    SEQUENCE,
+    NODE_SEQUENCE,
+    EMPTY_SEQUENCE,
+    ;
+
+}

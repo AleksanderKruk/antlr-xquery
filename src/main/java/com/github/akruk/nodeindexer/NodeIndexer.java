a84bb8871b7297674174744f89efda3d0c87efd6
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-08-01 04:20:32 +0200

    added Trees.getDescendants comment

--- src/main/java/com/github/akruk/nodeindexer/NodeIndexer.java
+++ src/main/java/com/github/akruk/nodeindexer/NodeIndexer.java
@@ -10,6 +10,7 @@ import org.antlr.v4.runtime.tree.Trees;
 
 public class NodeIndexer {
     Map<ParseTree, Integer> indexNodes(ParseTree tree) {
+        // Trees.getDescendants actually returns descendants-or-self
         var descendants = Trees.getDescendants(tree);
         Map<ParseTree, Integer> indexedNodes = new HashMap<>(descendants.size());
         int i = 0;

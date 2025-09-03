72d162e94d270b61124e0435385b8306ac60e187
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-09-03 18:39:27 +0200

    implemented lexer saving visited tokens

---
+++ src/main/java/com/github/akruk/antlrxquery/languageserver/AntlrXqueryLexerSavingTokens.java
@@ -0,0 +1,27 @@
+
+package com.github.akruk.antlrxquery.languageserver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.Token;
+
+import com.github.akruk.antlrxquery.AntlrXqueryLexer;
+
+public class AntlrXqueryLexerSavingTokens extends AntlrXqueryLexer {
+    public final List<Token> tokens = new ArrayList<>();
+    public AntlrXqueryLexerSavingTokens(CharStream input) {
+        super(input);
+    }
+
+    @Override
+    public Token getToken()
+    {
+        final Token token = super.getToken();
+        tokens.add(token);
+        return token;
+    }
+
+
+}

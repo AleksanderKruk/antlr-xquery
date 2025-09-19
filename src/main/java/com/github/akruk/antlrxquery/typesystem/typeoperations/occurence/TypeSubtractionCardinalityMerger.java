f8b8d9fb7cda8f75180279fa7f1653bcc99e8cfa
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-09-19 04:50:50 +0200

    implemented type subtraction cardinality merger

---
+++ src/main/java/com/github/akruk/antlrxquery/typesystem/typeoperations/occurence/TypeSubtractionCardinalityMerger.java
@@ -0,0 +1,63 @@
+package com.github.akruk.antlrxquery.typesystem.typeoperations.occurence;
+
+import com.github.akruk.antlrxquery.typesystem.defaults.XQueryCardinality;
+
+public class TypeSubtractionCardinalityMerger
+{
+    private static final int ZERO         = XQueryCardinality.ZERO.ordinal();
+    private static final int ONE          = XQueryCardinality.ONE.ordinal();
+    private static final int ZERO_OR_ONE  = XQueryCardinality.ZERO_OR_ONE.ordinal();
+    private static final int ONE_OR_MORE  = XQueryCardinality.ONE_OR_MORE.ordinal();
+    private static final int ZERO_OR_MORE = XQueryCardinality.ZERO_OR_MORE.ordinal();
+
+    protected final XQueryCardinality[][] automaton;
+
+    private final int occurenceCount = XQueryCardinality.values().length;
+
+    public TypeSubtractionCardinalityMerger()
+    {
+        this.automaton = getAutomaton();
+    }
+
+
+    private XQueryCardinality[][] getAutomaton() {
+        final var automaton = new XQueryCardinality[occurenceCount][occurenceCount];
+
+        automaton[ZERO][ZERO] = XQueryCardinality.ZERO;
+        automaton[ZERO][ONE] = XQueryCardinality.ZERO;
+        automaton[ZERO][ZERO_OR_ONE] = XQueryCardinality.ZERO;
+        automaton[ZERO][ZERO_OR_MORE] = XQueryCardinality.ZERO;
+        automaton[ZERO][ONE_OR_MORE] = XQueryCardinality.ZERO;
+
+        automaton[ONE][ZERO] = XQueryCardinality.ONE;
+        automaton[ONE][ONE] = XQueryCardinality.ZERO;
+        automaton[ONE][ZERO_OR_ONE] = XQueryCardinality.ZERO;
+        automaton[ONE][ZERO_OR_MORE] = XQueryCardinality.ZERO;
+        automaton[ONE][ONE_OR_MORE] = XQueryCardinality.ZERO;
+
+        automaton[ZERO_OR_ONE][ZERO] = XQueryCardinality.ZERO_OR_ONE;
+        automaton[ZERO_OR_ONE][ONE] = XQueryCardinality.ZERO;
+        automaton[ZERO_OR_ONE][ZERO_OR_ONE] = XQueryCardinality.ZERO;
+        automaton[ZERO_OR_ONE][ZERO_OR_MORE] = XQueryCardinality.ZERO;
+        automaton[ZERO_OR_ONE][ONE_OR_MORE] = XQueryCardinality.ZERO;
+
+        automaton[ZERO_OR_MORE][ZERO] = XQueryCardinality.ONE_OR_MORE; // 1+
+        automaton[ZERO_OR_MORE][ONE] = XQueryCardinality.ZERO_OR_MORE; // 0, 2+
+        automaton[ZERO_OR_MORE][ZERO_OR_ONE] = XQueryCardinality.ONE_OR_MORE; // 2+
+        automaton[ZERO_OR_MORE][ZERO_OR_MORE] = XQueryCardinality.ZERO;
+        automaton[ZERO_OR_MORE][ONE_OR_MORE] = XQueryCardinality.ZERO;
+
+        automaton[ONE_OR_MORE][ZERO] = XQueryCardinality.ONE_OR_MORE; // 1+
+        automaton[ONE_OR_MORE][ONE] = XQueryCardinality.ONE_OR_MORE; // 2+
+        automaton[ONE_OR_MORE][ZERO_OR_ONE] = XQueryCardinality.ONE_OR_MORE; // 2+
+        automaton[ONE_OR_MORE][ZERO_OR_MORE] = XQueryCardinality.ZERO;
+        automaton[ONE_OR_MORE][ONE_OR_MORE] = XQueryCardinality.ZERO;
+        return automaton;
+    }
+
+    public XQueryCardinality merge(XQueryCardinality o1, XQueryCardinality  o2) {
+        return automaton[o1.ordinal()][o2.ordinal()];
+    }
+
+
+}

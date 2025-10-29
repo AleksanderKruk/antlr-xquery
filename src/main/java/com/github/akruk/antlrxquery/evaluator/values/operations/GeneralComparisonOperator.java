8a7dc0643ef18abfdd09d5bc0d4be2b347f4a92d
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-10-29 23:21:13 +0100

    removed dead code

--- src/main/java/com/github/akruk/antlrxquery/evaluator/values/operations/GeneralComparisonOperator.java
+++ src/main/java/com/github/akruk/antlrxquery/evaluator/values/operations/GeneralComparisonOperator.java
@@ -11,14 +11,19 @@ public class GeneralComparisonOperator {
     private final ValueAtomizer atomizer;
     private final ValueComparisonOperator valueComparisonOperator;
 
-    public GeneralComparisonOperator(final XQueryValueFactory valueFactory, ValueAtomizer atomizer, ValueComparisonOperator valueComparisonOperator)
+    public GeneralComparisonOperator(
+        final XQueryValueFactory valueFactory,
+        final ValueAtomizer atomizer,
+        final ValueComparisonOperator valueComparisonOperator)
     {
         this.valueFactory = valueFactory;
         this.atomizer = atomizer;
         this.valueComparisonOperator = valueComparisonOperator;
     }
 
-    public XQueryValue generalEquals(final XQueryValue o1, final XQueryValue o2)
+    public XQueryValue generalEquals(
+        final XQueryValue o1,
+        final XQueryValue o2)
     {
         final List<XQueryValue> atomized1 = atomizer.atomize(o1);
         final List<XQueryValue> atomized2 = atomizer.atomize(o2);
@@ -26,28 +31,25 @@ public class GeneralComparisonOperator {
         for (final var element1 : atomized1) {
             for (final var element2 : atomized2) {
                 final var comparison = valueComparisonOperator.valueEquals(element1, element2);
-                if (comparison.isError)
+                if (comparison.isError || comparison.booleanValue)
                     return comparison;
-                if (comparison.booleanValue) {
-                    return valueFactory.bool(true);
-                }
             }
         }
         return valueFactory.bool(atomized1.size() == 0 && atomized2.size() == 0);
     }
 
-    public XQueryValue generalUnequals(final XQueryValue o1, final XQueryValue o2) {
+    public XQueryValue generalUnequals(
+        final XQueryValue o1,
+        final XQueryValue o2)
+    {
         final List<XQueryValue> atomized1 = atomizer.atomize(o1);
         final List<XQueryValue> atomized2 = atomizer.atomize(o2);
 
         for (final var element1 : atomized1) {
             for (final var element2 : atomized2) {
                 final var comparison = valueComparisonOperator.valueUnequal(element1, element2);
-                if (comparison.isError)
+                if (comparison.isError || comparison.booleanValue)
                     return comparison;
-                if (comparison.booleanValue) {
-                    return valueFactory.bool(true);
-                }
             }
         }
         return valueFactory.bool(atomized1.size() == 0 && atomized2.size() == 0);
@@ -55,155 +57,75 @@ public class GeneralComparisonOperator {
 
 
 
-    public XQueryValue generalLessThan(final XQueryValue o1, final XQueryValue o2) {
+    public XQueryValue generalLessThan(
+        final XQueryValue o1,
+        final XQueryValue o2)
+    {
         final List<XQueryValue> atomized1 = atomizer.atomize(o1);
         final List<XQueryValue> atomized2 = atomizer.atomize(o2);
 
         for (final var element1 : atomized1) {
             for (final var element2 : atomized2) {
                 final var comparison = valueComparisonOperator.valueLessThan(element1, element2);
-                if (comparison.isError)
+                if (comparison.isError || comparison.booleanValue)
                     return comparison;
-                if (comparison.booleanValue) {
-                    return valueFactory.bool(true);
-                }
             }
         }
         return valueFactory.bool(atomized1.size() == 0 && atomized2.size() == 0);
     }
 
 
-    public XQueryValue generalGreaterThan(final XQueryValue o1, final XQueryValue o2) {
+    public XQueryValue generalGreaterThan(
+        final XQueryValue o1,
+        final XQueryValue o2)
+    {
         final List<XQueryValue> atomized1 = atomizer.atomize(o1);
         final List<XQueryValue> atomized2 = atomizer.atomize(o2);
 
         for (final var element1 : atomized1) {
             for (final var element2 : atomized2) {
                 final var comparison = valueComparisonOperator.valueGreaterThan(element1, element2);
-                if (comparison.isError)
+                if (comparison.isError || comparison.booleanValue)
                     return comparison;
-                if (comparison.booleanValue) {
-                    return valueFactory.bool(true);
-                }
             }
         }
         return valueFactory.bool(atomized1.size() == 0 && atomized2.size() == 0);
     }
 
 
-    public XQueryValue generalLessEqual(final XQueryValue o1, final XQueryValue o2) {
+    public XQueryValue generalLessEqual(
+        final XQueryValue o1,
+        final XQueryValue o2)
+    {
         final List<XQueryValue> atomized1 = atomizer.atomize(o1);
         final List<XQueryValue> atomized2 = atomizer.atomize(o2);
 
         for (final var element1 : atomized1) {
             for (final var element2 : atomized2) {
                 final var comparison = valueComparisonOperator.valueLessEqual(element1, element2);
-                if (comparison.isError)
+                if (comparison.isError || comparison.booleanValue)
                     return comparison;
-                if (comparison.booleanValue) {
-                    return valueFactory.bool(true);
-                }
             }
         }
         return valueFactory.bool(atomized1.size() == 0 && atomized2.size() == 0);
     }
 
 
-    public XQueryValue generalGreaterEqual(final XQueryValue o1, final XQueryValue o2) {
+    public XQueryValue generalGreaterEqual(
+        final XQueryValue o1,
+        final XQueryValue o2)
+    {
         final List<XQueryValue> atomized1 = atomizer.atomize(o1);
         final List<XQueryValue> atomized2 = atomizer.atomize(o2);
 
         for (final var element1 : atomized1) {
             for (final var element2 : atomized2) {
                 final var comparison = valueComparisonOperator.valueGreaterEqual(element1, element2);
-                if (comparison.isError)
+                if (comparison.isError || comparison.booleanValue)
                     return comparison;
-                if (comparison.booleanValue) {
-                    return valueFactory.bool(true);
-                }
             }
         }
         return valueFactory.bool(atomized1.size() == 0 && atomized2.size() == 0);
     }
-
-
-
 }
 
-
-
-
-    // public XQueryValue generalEqual(final XQueryValue other) {
-	// }
-
-
-    // public XQueryValue generalUnequal(final XQueryValue other) {
-    //     final var thisAtomized = atomize();
-    //     final var otherAtomized = other.atomize();
-    //     for (final var thisElement : thisAtomized) {
-    //         for (final var otherElement : otherAtomized) {
-    //             if (thisElement.valueUnequal(otherElement).effectiveBooleanValue()) {
-    //                 return valueFactory.bool(true);
-    //             }
-    //         }
-    //     }
-    //     return valueFactory.bool(thisAtomized.size() == 0 && otherAtomized.size() == 0);
-	// }
-
-
-    // public XQueryValue generalLessThan(final XQueryValue other) {
-    //     final var thisAtomized = atomize();
-    //     final var otherAtomized = other.atomize();
-    //     for (final var thisElement : thisAtomized) {
-    //         for (final var otherElement : otherAtomized) {
-    //             if (thisElement.valueLessThan(otherElement).effectiveBooleanValue()) {
-    //                 return valueFactory.bool(true);
-    //             }
-    //         }
-    //     }
-    //     return valueFactory.bool(thisAtomized.size() == 0 && otherAtomized.size() == 0);
-
-	// }
-
-
-    // public XQueryValue generalLessEqual(final XQueryValue other) {
-    //     final var thisAtomized = atomize();
-    //     final var otherAtomized = other.atomize();
-    //     for (final var thisElement : thisAtomized) {
-    //         for (final var otherElement : otherAtomized) {
-    //             if (thisElement.valueLessEqual(otherElement).effectiveBooleanValue()) {
-    //                 return valueFactory.bool(true);
-    //             }
-    //         }
-    //     }
-    //     return valueFactory.bool(thisAtomized.size() == 0 && otherAtomized.size() == 0);
-
-	// }
-
-
-    // public XQueryValue generalGreaterThan(final XQueryValue other) {
-    //     final var thisAtomized = atomize();
-    //     final var otherAtomized = other.atomize();
-    //     for (final var thisElement : thisAtomized) {
-    //         for (final var otherElement : otherAtomized) {
-    //             if (thisElement.valueGreaterThan(otherElement).effectiveBooleanValue()) {
-    //                 return valueFactory.bool(true);
-    //             }
-    //         }
-    //     }
-    //     return valueFactory.bool(thisAtomized.size() == 0 && otherAtomized.size() == 0);
-	// }
-
-
-    // public XQueryValue generalGreaterEqual(final XQueryValue other) {
-    //     final var thisAtomized = atomize();
-    //     final var otherAtomized = other.atomize();
-    //     for (final var thisElement : thisAtomized) {
-    //         for (final var otherElement : otherAtomized) {
-    //             if (thisElement.valueGreaterEqual(otherElement).effectiveBooleanValue()) {
-    //                 return valueFactory.bool(true);
-    //             }
-    //         }
-    //     }
-    //     return valueFactory.bool(thisAtomized.size() == 0 && otherAtomized.size() == 0);
-	// }
